a4140da062484e2f7934db8b3221135cb85412c6
AssisrMatheus <assisr.matheus@gmail.com>
2020-10-26 08:28:10 -0300

    comments

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,17 +2,24 @@
 
 int main()
 {
+  // Print welcome messages to the terminal
   std::cout << "You are a secret agent breaking into a secure server room";
   std::cout << std::endl;
   std::cout << "Enter the correct code to continue...";
 
+  // Declare 3 number code
   const int a = 5;
   const int b = 10;
   const int c = 20;
 
+  /*
+    Declare the sum and the product
+    of our variables
+  */
   const int sum = a + b + c;
   const int product = a * b * c;
 
+  // Print sum and product to the terminal
   std::cout << std::endl;
   std::cout << sum << std::endl;
   std::cout << product;
