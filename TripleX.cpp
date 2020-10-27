54a0b8c10a1f9c82ec4582d7e127e8517e9ef318
AssisrMatheus <assisr.matheus@gmail.com>
2020-10-27 08:24:25 -0300

    function parameters

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
   // Print welcome messages to the terminal
-  std::cout << "\n\nYou are a secret agent breaking into a secure server room\n";
+  std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty << " secure server room\n";
   std::cout << "Enter the correct code to continue...\n\n";
 }
 
-bool PlayGame()
+bool PlayGame(int Difficulty)
 {
-  PrintIntroduction();
+  PrintIntroduction(Difficulty);
 
   // Declare 3 number code
   const int CodeA = 5;
@@ -51,11 +51,18 @@ bool PlayGame()
 
 int main()
 {
+  int LevelDifficulty = 1;
   while (true)
   {
-    bool bLevelComplete = PlayGame();
+    bool bLevelComplete = PlayGame(LevelDifficulty);
     std::cin.clear();
     std::cin.ignore();
+
+    if (bLevelComplete)
+    {
+      // Increase the level difficulty
+      ++LevelDifficulty;
+    }
   }
 
   return 0;
